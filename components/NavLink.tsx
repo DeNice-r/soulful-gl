ffe64489d9363c29ad1cb39241b4b38b246dbecc
Denys <ipzk211_kdyu@student.ztu.edu.ua>
2024-02-13 01:04:22 +0200

    fix: prevent hard navigation to the same link

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import Link from "next/link";
+import {usePathname} from "next/navigation";
 
-const MyLink = ({ href, children, ...props }: { href: string, children: any, props?: any }) => (
-  <Link href={href} passHref {...props}>
-    <Button component="a">
-      {children}
-    </Button>
-  </Link>
-);
+const MyLink = ({ href, children, ...props }: { href: string, children: any, props?: any }) => {
+    const pathname = usePathname();
+    const isActive = pathname === href;
+
+    return (
+        <Link href={isActive ? "" : href} passHref {...props}>
+            <Button variant={isActive ? 'text' : 'contained'}>
+                {children}
+            </Button>
+        </Link>
+    )
+};
 
 export default MyLink;
