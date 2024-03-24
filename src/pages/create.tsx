3ebec7b53168498abaf418c47c98c125b1f58e70
Anton-Cherniuk <zipzk211_cham@student.ztu.edu.ua>
2024-03-24 22:37:14 +0200

    feat(create.tsx): new form style

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -23,35 +23,51 @@ const Draft: React.FC = () => {
 
     return (
         <ConstrainedLayout>
-            <div>
-                <form onSubmit={submitData}>
-                    <h1>New Draft</h1>
-                    <input
-                        autoFocus
-                        onChange={(e) => setTitle(e.target.value)}
-                        placeholder="Title"
-                        type="text"
-                        value={title}
-                    />
-                    <textarea
-                        cols={50}
-                        onChange={(e) => setContent(e.target.value)}
-                        placeholder="Content"
-                        rows={8}
-                        value={content}
-                    />
-                    <input
-                        disabled={!content || !title}
-                        type="submit"
-                        value="Create"
-                    />
-                    <a
-                        className="back"
-                        href="#"
-                        onClick={() => Router.push('/')}
-                    >
-                        or Cancel
-                    </a>
+            <div className="flex h-full flex-col items-center justify-center gap-4">
+                <p className="text-center text-2xl font-bold">Новий Допис</p>
+                <form
+                    onSubmit={submitData}
+                    className="flex flex-col items-center justify-between gap-4 rounded-lg shadow-lg md:mb-20 md:h-1/2 md:w-4/5 md:bg-slate-100 md:py-8"
+                >
+                    <div className="flex h-2/3 flex-col gap-4 md:w-11/12 md:flex-row">
+                        <div className="flex basis-1/3 items-start">
+                            <div className="aspect-1.91/1 w-full rounded-lg border-2 bg-white shadow-inner"></div>
+                        </div>
+                        <div className="flex basis-2/3 flex-col gap-4">
+                            <input
+                                autoFocus
+                                onChange={(e) => setTitle(e.target.value)}
+                                placeholder="Заголовок"
+                                type="text"
+                                value={title}
+                                className="mx-0 rounded-lg border-2 border-solid p-2 active:border-slate-800"
+                            />
+                            <textarea
+                                cols={50}
+                                onChange={(e) => setContent(e.target.value)}
+                                placeholder="Вміст"
+                                rows={8}
+                                value={content}
+                                className="row-span-3 mx-0 flex-grow resize-none rounded-lg border-2 border-solid p-2 active:border-slate-800"
+                            />
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-center gap-4">
+                        <input
+                            disabled={!content || !title}
+                            type="submit"
+                            value="Створити"
+                            className="cursor-pointer rounded-md border-0 bg-sky-600 px-8 py-4 text-slate-50 transition-colors hover:bg-sky-500"
+                            // unactive bg-slate-200
+                        />
+                        <a
+                            className="rounded-md bg-slate-200 px-8 py-4 text-red-500 transition-colors hover:bg-red-600 hover:text-slate-50"
+                            href="#"
+                            onClick={() => Router.push('/')}
+                        >
+                            Відмінити
+                        </a>
+                    </div>
                 </form>
             </div>
             <style jsx>{`
@@ -61,25 +77,6 @@ const Draft: React.FC = () => {
                     display: flex;
                     justify-content: center;
                     align-items: center;
-                }
-
-                input[type='text'],
-                textarea {
-                    width: 100%;
-                    padding: 0.5rem;
-                    margin: 0.5rem 0;
-                    border-radius: 0.25rem;
-                    border: 0.125rem solid rgba(0, 0, 0, 0.2);
-                }
-
-                input[type='submit'] {
-                    background: #ececec;
-                    border: 0;
-                    padding: 1rem 2rem;
-                }
-
-                .back {
-                    margin-left: 1rem;
                 }
             `}</style>
         </ConstrainedLayout>
