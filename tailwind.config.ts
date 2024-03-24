44e6ec40c324b166939747cbedcbe175c543327a
Anton-Cherniuk <zipzk211_cham@student.ztu.edu.ua>
2024-03-24 22:24:51 +0200

    feat(tailwind): add fonts, aspect ratio and background setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,29 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['var(--font-sans)', ...fontFamily.sans],
+                sans: [
+                    '-apple-system',
+                    'BlinkMacSystemFont',
+                    '"Segoe UI"',
+                    'Roboto',
+                    'Helvetica',
+                    'Arial',
+                    'sans-serif',
+                    '"Apple Color Emoji"',
+                    '"Segoe UI Emoji"',
+                    '"Segoe UI Symbol"',
+                    ...fontFamily.sans,
+                ],
+            },
+            aspectRatio: {
+                '1.91/1': '191 / 100',
+            },
+            backgroundImage: {
+                'hero-pattern': `url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23c2cdd7' fill-opacity='0.51'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");`,
+                'homepage-cover': `url("../../public/images/homepage.svg");`,
+            },
+            colors: {
+                'light-blue': '#ebebf3',
             },
         },
     },
