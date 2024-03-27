e41bf1e7069cec6dd5bf39322317c8440b5bc6e1
Denys-Kalinovskyi <ipzk211_kdyu@student.ztu.edu.ua>
2024-03-27 02:39:01 +0200

    fix: add recommendation router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { postRouter } from '~/server/api/routers/post';
 import { exerciseRouter } from '~/server/api/routers/exercise';
 import { createCallerFactory, createTRPCRouter } from '~/server/api/trpc';
+import { recommendationRouter } from '~/server/api/routers/recommendation';
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createCallerFactory, createTRPCRouter } from '~/server/api/trpc';
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+    recommendation: recommendationRouter,
     post: postRouter,
     exercise: exerciseRouter,
 });
