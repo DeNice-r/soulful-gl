0181949d45974839a1e5c6ae42d3a8c0880102c1
Anton-Cherniuk <zipzk211_cham@student.ztu.edu.ua>
2024-03-28 15:10:15 +0200

    feature(landing):add responsive layout

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -10,28 +10,30 @@ const Post: React.FC<{ post: RouterOutputs['post']['get'][number] }> = ({
     const authorName = post.author ? post.author.name : 'Unknown author';
     return (
         <article
-            className="flex h-full flex-col items-center justify-between p-8 text-center text-inherit text-slate-800"
+            className="flex h-full flex-col items-center justify-between p-8 text-center text-inherit text-slate-800 md:text-left 2xl:text-center"
             onClick={() => Router.push('/post/[id]', `/post/${post.id}`)}
         >
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center gap-4 md:flex-row md:items-start md:gap-8 2xl:flex-col 2xl:items-center ">
                 {post.image && (
                     <Image
                         src={post.image}
-                        width={500}
-                        height={500}
+                        width={1920}
+                        height={1080}
                         alt={post.title}
-                        className="w-11/12 rounded"
+                        className="w-11/12 rounded object-contain md:w-1/3 2xl:w-11/12"
                     ></Image>
                 )}
-                <div className="divide-y divide-stone-700">
-                    <p className="py-4 text-3xl font-bold">{post.title}</p>
-                    <ReactMarkdown className={'py-4'}>
+                <div className="divide-stone-700 md:w-2/3 md:divide-y">
+                    <p className="text-lg font-bold md:pb-4 md:text-xl 2xl:text-3xl">
+                        {post.title}
+                    </p>
+                    <ReactMarkdown className={'py-2 md:py-4'}>
                         {post.description}
                     </ReactMarkdown>
                 </div>
             </div>
-            <div className="self-end rounded bg-stone-700 px-2 py-1 md:mt-4">
-                <small className="font-light text-neutral-200">
+            <div className="self-end rounded bg-stone-700 px-1 md:px-2 md:py-1 2xl:mt-4">
+                <small className="text-xs font-light text-neutral-200">
                     {authorName}
                 </small>
             </div>
