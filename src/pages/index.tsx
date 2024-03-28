568314fc67a6f270dc654b402488ab093bcdc7b0
Anton-Cherniuk <zipzk211_cham@student.ztu.edu.ua>
2024-03-28 21:28:53 +0200

    feat(index.tsx):add inspiration block

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,17 +32,36 @@ const Blog: React.FC = () => {
                     </section>
                 </div>
             </div>
-            <article className="flex w-full flex-col justify-center gap-8 py-16 shadow-inner md:px-60 2xl:flex-row 2xl:flex-wrap 2xl:gap-32 2xl:px-40">
+            <article className="flex w-full flex-col justify-center gap-8 py-16 shadow-inner md:px-80 2xl:flex-row 2xl:flex-wrap 2xl:gap-32 2xl:px-40">
                 {posts.data &&
                     posts.data.map((post) => (
-                        <article
+                        <div
                             key={post.id}
                             className="flex-1 justify-center bg-blue-200 transition-shadow duration-100 ease-in hover:cursor-pointer hover:shadow md:rounded 2xl:min-w-72 2xl:max-w-80"
                         >
                             <Post post={post} />
-                        </article>
+                        </div>
                     ))}
             </article>
+            <div className="bg-article-cover flex items-center bg-neutral-200 py-12 text-center drop-shadow-md md:h-auto md:justify-center md:p-32">
+                <article className="flex flex-col gap-8 bg-neutral-300 bg-opacity-40 p-10 md:w-1/2 md:rounded-xl">
+                    <h1 className="text-xl font-bold text-stone-800 md:text-4xl">
+                        Чим ми надихались
+                    </h1>
+                    <p className="flex-grow font-medium text-stone-800 md:text-xl">
+                        Maecenas tristique sagittis quam sit amet scelerisque.
+                        Sed vel fermentum felis, quis tristique ex. Pellentesque
+                        at velit efficitur, faucibus odio eget, pretium nisl.
+                        Duis ut viverra purus. Proin lectus purus, congue eu
+                        feugiat ut, cursus a ex. Vivamus mattis turpis sed ipsum
+                        tincidunt, non finibus lectus hendrerit. In sit amet
+                        quam mi. Sed ullamcorper ipsum magna. Sed efficitur,
+                        tortor eu rutrum fermentum, ipsum eros tristique dui,
+                        non blandit velit dui in nisi. Etiam quam nisl, mattis
+                        ac justo sit amet, luctus suscipit purus.
+                    </p>
+                </article>
+            </div>
         </main>
     );
 };
