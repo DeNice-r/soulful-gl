4a39386a93802d2d32c1d23aba57965939262b2c
Anton-Cherniuk <zipzk211_cham@student.ztu.edu.ua>
2024-04-01 01:01:18 +0300

    feat(header): start changing header style and logic

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -18,16 +18,12 @@ const MyLink = ({
 
     return (
         <Link
-            className={`${className} ${isActive ? 'bg-neutral-200' : 'bg-neutral-400'} grow basis-1`}
+            className={`${className} ${isActive ? 'font-bold' : 'font-normal'} text-lg`}
             href={isActive ? '' : href}
             passHref
             {...props}
         >
-            <button
-                className={`${isActive ? 'nav-btn-active' : 'nav-btn-primary'} ${className} w-full`}
-            >
-                {children}
-            </button>
+            {children}
         </Link>
     );
 };
