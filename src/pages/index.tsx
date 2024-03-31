fb2352c3ba61a60b584a117438b1d659b436d09b
Anton-Cherniuk <zipzk211_cham@student.ztu.edu.ua>
2024-04-01 00:19:50 +0300

    style(landing): redesign layout without altering content blocks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,41 +13,38 @@ const Blog: React.FC = () => {
     });
     return (
         <main className="flex w-full flex-col">
-            <div className="bg-homepage-cover h-svh bg-cover">
-                <div className="flex h-full flex-col bg-neutral-200 bg-opacity-75">
-                    <Header />
-                    <section className="flex h-full flex-col">
-                        <h1
-                            className={`${truculenta.className} flex flex-grow items-center justify-center text-3xl text-cyan-800 md:text-6xl`}
-                        >
-                            Soulful
-                        </h1>
-                        <p className="w-3/4 flex-grow self-center text-center text-lg font-medium md:w-1/2 md:text-3xl">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. Praesent ornare tortor ac elementum ultricies.
-                            Donec sit amet tempor est, at placerat tellus.
-                            Vestibulum ut risus placerat, vehicula velit eget,
-                            aliquet elit. Phasellus vel sodales libero.
-                            Suspendisse a vestibulum lorem. Suspendisse libero
-                            quam, suscipit sit amet metus pulvinar, placerat
-                            lacinia eros.
-                        </p>
-                    </section>
-                </div>
+            <div className=" flex h-svh flex-col">
+                <Header />
+                <section className="flex h-full flex-col items-center justify-center md:gap-16">
+                    <h1
+                        className={`${truculenta.className} flex items-center justify-center text-3xl text-cyan-800 md:text-6xl`}
+                    >
+                        Soulful
+                    </h1>
+                    <p className="w-3/4 self-center text-center text-lg font-medium md:w-1/2 md:text-3xl">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Praesent ornare tortor ac elementum ultricies. Donec sit
+                        amet tempor est, at placerat tellus. Vestibulum ut risus
+                        placerat, vehicula velit eget, aliquet elit. Phasellus
+                        vel sodales libero. Suspendisse a vestibulum lorem.
+                        Suspendisse libero quam, suscipit sit amet metus
+                        pulvinar, placerat lacinia eros.
+                    </p>
+                </section>
             </div>
-            <article className="flex w-full flex-col justify-center gap-8 py-16 shadow-inner md:px-80 2xl:flex-row 2xl:gap-32 2xl:px-40">
+            <article className="flex w-full flex-col justify-center gap-8 py-16 md:px-80 2xl:flex-row 2xl:gap-32 2xl:px-40">
                 {posts.data &&
                     posts.data.map((post) => (
                         <div
                             key={post.id}
-                            className="flex-1 justify-center bg-blue-200 transition-shadow duration-100 ease-in hover:cursor-pointer hover:shadow md:rounded 2xl:min-w-72 2xl:max-w-80"
+                            className="flex-1 justify-center bg-neutral-300 bg-opacity-80 shadow-md outline outline-neutral-400 transition-shadow duration-100 ease-in hover:cursor-pointer hover:shadow-lg md:rounded-md"
                         >
                             <Post post={post} />
                         </div>
                     ))}
             </article>
-            <div className="bg-article-cover flex items-center bg-neutral-200 py-12 text-center drop-shadow-md md:h-auto md:justify-center md:p-32">
-                <article className="flex flex-col gap-8 bg-neutral-300 bg-opacity-40 p-10 md:w-1/2 md:rounded-xl">
+            <div className="flex items-center py-12 text-center md:h-auto md:justify-center md:p-32">
+                <article className="flex flex-col gap-8 bg-neutral-300 bg-opacity-70 p-10 drop-shadow-lg md:w-1/2 md:rounded-xl">
                     <h1 className="text-xl font-bold text-stone-800 md:text-4xl">
                         Чим ми надихались
                     </h1>
@@ -79,7 +76,7 @@ const Blog: React.FC = () => {
                         </div>
                         <div className="flex items-center justify-center md:basis-1/3">
                             <Image
-                                src="images/question-mark-svgrepo-com.svg"
+                                src="images/question-mark.svg"
                                 alt="question mark"
                                 height={400}
                                 width={400}
