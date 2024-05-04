256f29d2c5e4aa75b2d10ab86013c38032352007
Denys-Kalinovskyi <ipzk211_kdyu@student.ztu.edu.ua>
2024-05-04 17:29:54 +0300

    feat: show user id in chat

--- src/components/chat/ChatMessageWindow.tsx
+++ src/components/chat/ChatMessageWindow.tsx
@@ -30,6 +30,11 @@ export const ChatMessageWindow = ({
                 bgcolor: 'grey.400',
             }}
         >
+            <div className="bg-gray-300">
+                <span className="">Листування з </span>
+                <code className="">{chats[currentChat]?.userId}</code>
+            </div>
+            {/* a chat header? */}
             <Box sx={{ flexGrow: 1, overflow: 'auto', p: 2 }}>
                 {chats[currentChat]?.messages?.map((message) => (
                     <ChatMesssage key={message.id} message={message} />
