c2d0f4c4eaf4c5e34fde111602ce51d1d2c594e2
Denys-Kalinovskyi <ipzk211_kdyu@student.ztu.edu.ua>
2024-05-11 02:02:18 +0300

    fix: operators out of sync with users view

--- src/components/management/Operators.tsx
+++ src/components/management/Operators.tsx
@@ -1,26 +1,43 @@
-import type React from 'react';
+import React, { useState } from 'react';
 import { UsersView } from '~/components/management/UsersView';
 import { api } from '~/utils/api';
-import { DEFAULT_LIMIT } from '~/utils/constants';
+import { DEFAULT_LIMIT, NO_REFETCH } from '~/utils/constants';
 import { useRouter } from 'next/router';
 
 export const Operators: React.FC = () => {
     const router = useRouter();
+    const queryParam = router.query.query;
+    const orderBy = router.query.orderBy as string | undefined;
+    const order = router.query.order as string | undefined;
+
+    const [query, setQuery] = useState<string | undefined>(
+        queryParam as string,
+    );
 
     const limit = router.query.limit
         ? Number(router.query.limit)
         : DEFAULT_LIMIT;
     const page = router.query.page ? Number(router.query.page) : 1;
 
-    const users = api.user.list.useQuery({
-        limit,
-        page,
-        permissions: ['chat:*'],
-    });
+    const users = api.user.list.useQuery(
+        { limit, page, query, orderBy, order, permissions: ['chat:*'] },
+        NO_REFETCH,
+    );
 
     const total = users.data?.count ? Math.ceil(users.data.count / limit) : 0;
 
     return (
-        <UsersView {...{ usersQuery: users, page, total, router }}></UsersView>
+        <UsersView
+            {...{
+                usersQuery: users,
+                page,
+                total,
+                router,
+                query,
+                setQuery,
+                orderBy,
+                order,
+            }}
+        ></UsersView>
     );
 };
