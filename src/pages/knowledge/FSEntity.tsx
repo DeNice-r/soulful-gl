c06971d278f48e2ac1cd2a2d9cca05f3977563f7
Anton-Cherniuk <zipzk211_cham@student.ztu.edu.ua>
2024-05-14 10:46:47 +0300

    fix(FSEntiity): export default

--- src/pages/knowledge/FSEntity.tsx
+++ src/pages/knowledge/FSEntity.tsx
@@ -4,7 +4,7 @@ import { Button } from '~/components/ui/button';
 import { Input } from '~/components/ui/input';
 import { type RouterOutputs } from '~/utils/api';
 
-export const FSEntity: React.FC<{
+const FSEntity: React.FC<{
     entity: RouterOutputs['documentFolder']['list'][
         | 'folders'
         | 'documents'][number];
@@ -45,3 +45,5 @@ export const FSEntity: React.FC<{
         </Button>
     );
 };
+
+export default FSEntity;
