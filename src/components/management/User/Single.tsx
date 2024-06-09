6b73eb63cf924b064f7328222bbfef1677224f17
Denys-Kalinovskyi <ipzk211_kdyu@student.ztu.edu.ua>
2024-06-09 14:10:09 +0300

    chore: replace custom svg with lib-provided

--- src/components/management/User/Single.tsx
+++ src/components/management/User/Single.tsx
@@ -10,6 +10,7 @@ import { Button } from '../../ui/button';
 import { defaultFormatDateTime } from '~/utils/dates';
 import { useToast } from '~/components/ui/use-toast';
 import Image from 'next/image';
+import { MoreHorizontalIcon } from 'lucide-react';
 
 export const Single: React.FC<{
     entity: RouterOutputs['user']['list']['values'][number];
@@ -130,26 +131,3 @@ export const Single: React.FC<{
         </>
     );
 };
-
-function MoreHorizontalIcon(
-    props: React.JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>,
-) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <circle cx="12" cy="12" r="1" />
-            <circle cx="19" cy="12" r="1" />
-            <circle cx="5" cy="12" r="1" />
-        </svg>
-    );
-}
